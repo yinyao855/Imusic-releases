7f83419c700862f7566bc59be62a37f0eb870bfa
薛惠天 <14160885+xue-ht@user.noreply.gitee.com>
2024-04-27 14:06:30 +0800

    加歌

--- imusic_frontend/imusic-router/src/views/CurrentUser_SongList.vue
+++ imusic_frontend/imusic-router/src/views/CurrentUser_SongList.vue
@@ -1,7 +1,22 @@
 <script setup>
 import {defineModel} from "vue"
+import axios from "axios";
 const needtoaddSongid=defineModel('needtoaddSongid')
 const CurrentUser_SongListdata = defineModel('CurrentUser_SongListdata')
+const addtosonglist=(index)=>{
+  const songlistid=CurrentUser_SongListdata.value[index].id;
+  const formData=new FormData();
+  formData.append('song_id',needtoaddSongid.value);
+  formData.append('songlist_id',songlistid);
+  axios.post('http://182.92.100.66:5000/songlists/addsong',formData)
+      .then(response=>{
+        console.log(response.data);
+        alert('歌曲添加成功');
+      })
+      .catch(error=>{
+        console.log(error.response.data);
+      })
+}
 </script>
 
 <template>
