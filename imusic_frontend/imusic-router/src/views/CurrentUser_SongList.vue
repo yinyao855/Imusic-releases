504cf8c199d58be200e08156d79cff5492f6759f
薛惠天 <14160885+xue-ht@user.noreply.gitee.com>
2024-04-27 14:42:30 +0800

    功能

--- imusic_frontend/imusic-router/src/views/CurrentUser_SongList.vue
+++ imusic_frontend/imusic-router/src/views/CurrentUser_SongList.vue
@@ -23,9 +23,9 @@ const addtosonglist=(index)=>{
   <div class="mx-auto text-white text-3xl py-4 w-full text-center" style="height:10%">我创建的歌单</div>
   <div class="w-full flex" style="height:90%">
     <div class="w-full" v-for="(item, index) in CurrentUser_SongListdata" :key="index">
-      <div class="w-1/4">
-        <img :src="item.cover" alt="歌单封面" class="w-full" @click="addtosonglist(index)">
-        <span>{{ item.title }}</span>
+      <div class="w-64">
+        <img :src="item.cover" alt="歌单封面" class=" aspect-square rounded-2xl" @click="addtosonglist(index)">
+        <div class="w-64 text-xl text-center text-white">{{ item.title }}</div>
       </div>
     </div>
   </div>
