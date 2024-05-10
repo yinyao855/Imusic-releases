3387fa4f41637fd0ed94fc5f50f84e5c74be2fa1
薛惠天 <14160885+xue-ht@user.noreply.gitee.com>
2024-05-10 15:34:25 +0800

    探索关注

--- imusic_frontend/imusic-router/src/views/Explore/ExplorePage_Main.vue
+++ imusic_frontend/imusic-router/src/views/Explore/ExplorePage_Main.vue
@@ -3,6 +3,7 @@ import {computed, ref} from "vue";
 import Singer_Show_Area from "@/views/Explore/Songs_Show_Area.vue";
 import Search from "@/components/Search.vue";
 import List_Show_Area from "@/views/Explore/List_Show_Area.vue";
+import User_Show_Area from "@/views/Explore/User_Show_Area.vue";
 
 const token = defineModel('token')
 const emits = defineEmits(['handlePlayAfter', 'handlePlayNow']);
@@ -15,6 +16,10 @@ const NaviClass1 = computed(() => ({
 const NaviClass2 = computed(() => ({
   'text-base inline-block mx-5 w-30 rounded-lg antialiased tracking-widest font-medium transition-colors duration-400 hover:bg-gray-600/40': true,
   'text-cyan-700 underline underline-offset-8 decoration-2': NaviMode.value === '2',
+}));
+const NaviClass3 = computed(() => ({
+  'text-base inline-block mx-5 w-30 rounded-lg antialiased tracking-widest font-medium transition-colors duration-400 hover:bg-gray-600/40': true,
+  'text-cyan-700 underline underline-offset-8 decoration-2': NaviMode.value === '3',
 }));
 const changeNaviMode = (newMode) => {
   NaviMode.value = newMode.toString();
@@ -38,11 +43,13 @@ const handlePlayNow = (index) => {
   <div class="w-full h-16 pl-6 fixed z-50 bg-zinc-900">
     <div :class="[NaviClass1, 'text-transition']" @click="changeNaviMode(1)" style="line-height: 56px">歌曲</div>
     <div :class="[NaviClass2, 'text-transition']" @click="changeNaviMode(2)" style="line-height: 56px">歌单</div>
+    <div :class="[NaviClass3, 'text-transition']" @click="changeNaviMode(3)" style="line-height: 56px">用户</div>
   </div>
   <div class="w-full mt-16">
     <Singer_Show_Area v-model:username="username" v-if="NaviMode==='1'" v-model:token="token"
                       @handlePlayAfter="handlePlayAfter" @handlePlayNow="handlePlayNow"></Singer_Show_Area>
     <List_Show_Area v-if="NaviMode==='2'" v-model:token="token" v-model:username="username" @handlePlayAfter="handlePlayAfter" @handlePlayNow="handlePlayNow"></List_Show_Area>
+    <User_Show_Area v-if="NaviMode==='3'" v-model:username="username" v-model:token="token"></User_Show_Area>
   </div>
 </template>
 
