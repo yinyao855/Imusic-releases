8734cfcfb5fb767c98d4007b729faed5ca16d5c3
牛梓涵 <12308755+answer-coarse@user.noreply.gitee.com>
2024-05-23 15:32:42 +0800

    message loading

--- imusic_frontend/imusic-router/src/views/Home.vue
+++ imusic_frontend/imusic-router/src/views/Home.vue
@@ -49,7 +49,7 @@ const autoLogin = () => {
           console.log("auto login success!");
           // GetMessage();
           getsonglistinit();
-          intervalId = setInterval(GetMessage, 10000); // 10000 毫秒即 10 秒
+          intervalId = setInterval(GetMessage, 5000); // 10000 毫秒即 10 秒
         }
         else{
           console.log("auto login failed!");
