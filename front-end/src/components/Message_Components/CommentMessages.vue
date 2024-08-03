4ee911828b3e54795721b2ebd2aab29b6a5a8eff
张旖霜 <yeestiew@gmail.com>
2024-08-03 13:47:01 +0800

    CommentMessages简单实现界面

--- front-end/src/components/Message_Components/CommentMessages.vue
+++ front-end/src/components/Message_Components/CommentMessages.vue
@@ -6,17 +6,29 @@ import { CommentMessageList } from '@/js/Message.js'
 
 <template>
   <div class="w-full h-full">
-    <div class="overflow-auto">
-      <div class="h-32 w-full" v-for="(Message,index) in CommentMessageList" :key="index">
-        <img :src="Message.Avatar" alt="头像"
-             class="h-14 rounded-xl aspect-square inline-block -ml-3"/>
-        <svg v-if="!Message.is_read" class="h-2 w-2 text-red-500 inline-block rounded-full align-top" width="24" height="24" viewBox="0 0 24 24" stroke-width="1"
-             stroke="currentColor" fill="red" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z"/>
-          <circle cx="12" cy="12" r="4"/>
-        </svg>
+    <div class="w-full h-full overflow-auto block bg-[#faf3ee]">
+      <div class="w-full border-t"></div>
+      <div class="w-full border-b h-20 flex hover:bg-gray-300/50 transition-colors ease-in duration-300"
+           v-for="(Message,index) in CommentMessageList" :key="index" onclick="report.showModal()"
+           @click="">
+        <img :src="Message.message_cover" alt="头像"
+             class="h-16 rounded-xl aspect-square inline-block my-auto ml-2" />
+        <div class="w-4">
+          <img v-if="!Message.is_read" src="../icons/Message_Unread_Icon.svg" alt="未读红点"
+               class="h-2 w-2 inline-block rounded-full align-top mt-1"/>
+        </div>
+        <div class="my-auto ml-2 flex-1 w-20">
+          <div class="font-bold text-lg text-gray-600 mb-2">
+            {{ Message.sender }}
+          </div>
+          <div class="text-sm truncate ...">
+            {{ Message.content }}
+          </div>
+        </div>
+        <div class="my-auto mr-2">
+          {{ Message.send_date }}
+        </div>
       </div>
-      {{CommentMessageList.length}}
       <div class="h-32 w-full" v-if="MusicPlayerVisible"></div>
     </div>
   </div>
